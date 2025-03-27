fab750353e8c70674a91c0ed7af4b960ecc5db09
PatrickHirsch <45246344+PatrickHirsch@users.noreply.github.com>
2025-03-27 00:46:11 -0400

    Update HirschP2_m6a1.py
    In helping a peer with the assignment I realized I had overlooked formating details for the output.  Revised accordingly and resubmitted.

--- Module6/Assignment1/HirschP2_m6a1.py
+++ Module6/Assignment1/HirschP2_m6a1.py
@@ -2,7 +2,7 @@
 # HirschP2
 # Module 6: Assignment 1 -- Reading and Writing Text Files
 # CIT 383 | Scripting I
-# 2025/03/17
+# 2025/03/17 - Revised: 2025/03/27
 
 import csv
 
@@ -22,10 +22,10 @@ def read_user_data():
     return ret
 
 # Given an array of dictionaries supplied by read_user_data(), interpret it handling data accordingly:
-#   For each row, if the login attempts exceed the acceptable threshold print name and copy to output file.
-#   Returns the number of suspicious attempts
+#   For each row, if the login attempts exceed the acceptable threshold write appropriate data to list to return.
+#   returns list and writes to output file
 def write_suspicious_logins_data(userData):
-    ret=0
+    sus=[['name','login_count','login_count_excess']]
     with open(FILENAME_OUTPUT,'w',newline='') as file:
         
         # Check the headers to ensure input file matches expected specification, 
@@ -33,18 +33,21 @@ def write_suspicious_logins_data(userData):
         headers=userData[0].keys()
         if list(headers)!=CSV_HEADERS:
             raise NameError("Headers not as expected.")
-        CSVWriter=csv.DictWriter(file,fieldnames=headers)
-        CSVWriter.writeheader()
+        CSVWriter=csv.writer(file)
         
         # Iterate through each row of input data and determine if login_count exceeds MIN_ACCESSIBLE_ATTEMPTS
         #   Where true, write to output file, print name, and increment count
         for i in userData:
             if int(i[CSV_HEADERS[2]])>MIN_ACCESSIBLE_ATTEMPTS:
-                CSVWriter.writerow(i)
-                print(f"Suspicious logins from: {i[CSV_HEADERS[0]]} {i[CSV_HEADERS[1]]}")
-                ret+=1
-    return ret
+                sus.append([i[CSV_HEADERS[1]]+'; '+i[CSV_HEADERS[0]], i[CSV_HEADERS[2]], int(i[CSV_HEADERS[2]])-100])
+                
+        CSVWriter.writerows(sus)
+    return sus
 
+# Given a 2D array, print as a table with column widths suited to expected output of write_suspicious_logins_data()
+def printTable(list2D):
+    for row in list2D:
+        print(f"{row[0]:20s}\t {row[1]:10s}\t {str(row[2]):10s}\t ")
 
 def main_HirschP2():
     # Run read_user_data() to read input file.
@@ -61,7 +64,7 @@ def main_HirschP2():
     # Run write_suspicious_logins_data() to process data.
     #   Exits gracefully w/ error message if output file cannot be written to or data is not as expected.
     try:
-        count=write_suspicious_logins_data(userData)
+        suspicious=write_suspicious_logins_data(userData)
     except PermissionError:
         print(f'ERROR: Could not write to output file, {FILENAME_INPUT}.')
         exit()
@@ -69,7 +72,8 @@ def main_HirschP2():
         print(f'ERROR: Input data formated incorrectly, {FILENAME_INPUT}.')
         exit()
     
-    print(f'\n{count} total employees with suspicious login attempts.')
+    printTable(suspicious)
+    print(f'\n{len(suspicious)-1} total employees with suspicious login attempts.')
 
 
 main_HirschP2()
